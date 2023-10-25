b8858a9e4c86563c685f35dac1e7c8621fe0b722
DINESH THIRU <dineshthiru36@gmail.com>
2023-10-25 20:55:29 +0530

    Updated

--- 35.Search-insert-position.js
+++ 35.Search-insert-position.js
@@ -2,19 +2,17 @@
 
 // 35. Search Insert Position
 
-var nums = [1, 3, 5, 6]
-var target = 2
 
+var nums = [1, 3, 5, 6]
+var target = 7
 
 function searchInsert(nums, target) {
-
-  nums.push(target)
-
-  nums.sort((a, b) => a - b);
-  
-  console.log(nums)
-
-  return nums.indexOf(target)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target ||  nums[i] > target) {
+      return i
+    }
+  }
+  return nums.length
 }
 
 console.log(searchInsert(nums, target))
@@ -33,13 +31,17 @@ console.log(searchInsert(nums, target))
 
 
 
-
-
+// var nums = [1, 3, 5, 6]
+// var target = 2
 // function searchInsert(nums, target) {
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] === target ||  nums[i] > target) {
-//       return i
-//     }
-//   }
-//   return nums.length
+
+//   nums.push(target)
+
+//   nums.sort((a, b) => a - b);
+  
+//   // console.log(nums)
+
+//   return nums.indexOf(target)
 // }
+
+// console.log(searchInsert(nums, target))
